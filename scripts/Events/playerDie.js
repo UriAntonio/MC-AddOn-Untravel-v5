11d1fcaad5fb6074792f6c5891afe58fd9f47bb2
UriAntonio <74329787+UriAntonio@users.noreply.github.com>
2023-11-04 17:58:48 -0600

    corrected int output

--- scripts/Events/playerDie.js
+++ scripts/Events/playerDie.js
@@ -10,7 +10,7 @@ Server.Minecraft.world.afterEvents.entityDie.subscribe((data) => {
       dimension: player.dimension.id
     }
     Server.BackDB.set(player.name, backData)
-    player.sendMessage(`§c${player.location.x}, ${player.location.y}, ${player.location.z}`)
+    player.sendMessage(`§c${Math.floor(player.location.x)}, ${Math.floor(player.location.y)}, ${Math.floor(player.location.z)}`)
 
   }
 })
