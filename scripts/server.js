0e7e46806902326d68764436894b8e81a78f3d86
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-04 19:10:18 +0000

    added a world inicializite

--- scripts/server.js
+++ scripts/server.js
@@ -1,12 +1,16 @@
-import  * as mc  from "@minecraft/server";
+import * as mc from "@minecraft/server";
 import Config from "./conf/Configuration";
+//
+//
+import { Database } from "./modules/DataBase/Database";
 
 
 const Event = [
   "chatSend",
   "playerSpawn",
-  "itemuse",
-  "playerleave"
+  "itemUse",
+  "playerLeave",
+  "playerDie"
 ]
 
 
@@ -18,7 +22,7 @@ class ServerClass {
     this.PlayerOnline = {}
     //this.HomeDB = new Database("homeDB")
     //this.WarpDB = new Database("warpDB")
-    //this.BackDB = new Database("backDB")
+    this.BackDB = new Database("backDB")
     //this.BanDB = new Database("banDB")
     //this.Setting = new Setting()
     //this.Money = Money
@@ -30,52 +34,72 @@ class ServerClass {
     //this.timeStarted = Date.now()
 
   }
-/**
-   * Obtiene al Jugadorpor Nombre
-   * @param {string} targetName 
-   * @returns {PlayerClass}
-   */
-getPlayer(targetName) {
-  return this.world.getAllPlayers().find(player => player.name == targetName)
-}
+  /**
+     * Obtiene al Jugadorpor Nombre
+     * @param {string} targetName 
+     * @returns {PlayerClass}
+     */
+  getPlayer(targetName) {
+    return this.world.getAllPlayers().find(player => player.name == targetName)
+  }
 
-/**
-   * Obtiene la Dimension por string
-   * @param {string} dimensionId 
-   * @returns {mc.Dimension}
-   */
-getDimension(dimensionId) {
-  return this.world.getDimension(dimensionId)
-}
+  /**
+     * Obtiene la Dimension por string
+     * @param {string} dimensionId 
+     * @returns {mc.Dimension}
+     */
+  getDimension(dimensionId) {
+    return this.world.getDimension(dimensionId)
+  }
 
-/**
-   * Obtiene el Inventory de la Entity
-   * @param {mc.Entity} entity 
-   * @returns {mc.Container}
-   */
-getInventory(entity) {
-  return entity.getComponent("minecraft:inventory").container
-}
+  /**
+     * Obtiene el Inventory de la Entity
+     * @param {mc.Entity} entity 
+     * @returns {mc.Container}
+     */
+  getInventory(entity) {
+    return entity.getComponent("minecraft:inventory").container
+  }
 
-/**
- * Ejecuta comando como world
- * @param {string} command 
- * @param {string} dimension 
- * @returns {mc.CommandResult}
- */
-async runCommand(command, dimension) {
-  let res = await this.getDimension(dimension ?? "minecraft:overworld").runCommandAsync(command)
-  return res
-}
+  /**
+   * Ejecuta comando como world
+   * @param {string} command 
+   * @param {string} dimension 
+   * @returns {mc.CommandResult}
+   */
+  async runCommand(command, dimension) {
+    let res = await this.getDimension(dimension ?? "minecraft:overworld").runCommandAsync(command)
+    return res
+  }
+  async waitLoaded() {
+    return new Promise((resolve) => {
+      let systemId = mc.system.runInterval(() => {
+        let ent = this.world.getAllPlayers()
+        if (ent.length > 0) {
+          this.isLoaded = true
+          mc.system.clearRun(systemId)
+          resolve()
+        }
+      }, 10)
+    })
+  }
 }
 
 
 const Server = new ServerClass()
 
+Server.world.afterEvents.worldInitialize.subscribe(async (data) => {
+  //const date = new Date.now()
+  //await Server.waitLoaded()
+  //
+  Event.forEach(event => {
+    import(`./Events/${event}`)
+  })
+})
 
 mc.system.beforeEvents.watchdogTerminate.subscribe(data => {
-    data.cancel = true
-    SystemLog(`Watchdog Terminate: ${data.terminateReason}`)
-  })
+  data.cancel = true
+  SystemLog(`Watchdog Terminate: ${data.terminateReason}`)
+})
 
 export default Server
