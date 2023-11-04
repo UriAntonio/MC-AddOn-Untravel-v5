3e99fb9e02cfe5a0f3e691e7e45fe95c60c4d1c7
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-04 19:14:16 +0000

    set itenUses as a default module

--- scripts/Events/itemUse.js
+++ scripts/Events/itemUse.js
@@ -1,13 +1,12 @@
 import { welcome } from "../util/newMemberMessage";
 import Server from "../server";
 
-export const itemUses = () => {
-    Server.world.beforeEvents.itemUse.subscribe((data) => {
-        const source = data.source
-        const items = data.itemStack
-        if (items.typeId === "mx:new" &&
+
+Server.world.beforeEvents.itemUse.subscribe((data) => {
+    const source = data.source
+    const items = data.itemStack
+    if (items.typeId === "mx:new" &&
         !source.hasTag("online")) {
-            welcome(source);
-          }
-    })
-}
+        welcome(source);
+    }
+})
