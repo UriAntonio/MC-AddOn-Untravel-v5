1a63aba9356f9ce2f522b54e935090a3afeeb67f
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-04 14:46:59 +0000

    OnJoinData changed path

--- scripts/Events/playerSpawn.js
+++ scripts/Events/playerSpawn.js
@@ -1,4 +1,4 @@
-import { onJoinData } from "../modules/onJoinData.js";
+import { onJoinData } from "../modules/Data/onJoinData.js";
 import { welcome } from "../util/newMemberMessage.js";
 import Config from "../conf/Configuration.js";
 import Server from "../server.js";
