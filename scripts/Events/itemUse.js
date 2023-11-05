b825c5052895865348b90aa232fa20172ff35487
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-05 05:00:48 +0000

    testing item use

--- scripts/Events/itemUse.js
+++ scripts/Events/itemUse.js
@@ -1,12 +1,13 @@
 import { welcome } from "../util/newMemberMessage";
 import Server from "../server";
+import Config from "../conf/Configuration";
 
 
 Server.world.beforeEvents.itemUse.subscribe((data) => {
     const source = data.source
     const items = data.itemStack
     if (items.typeId === "mx:new" &&
-        !source.hasTag("online")) {
+        source.hasTag(Config.AdminTag)) {
         welcome(source);
     }
 })
