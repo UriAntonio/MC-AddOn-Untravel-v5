766423635d17dd552c25adb289e946f813fcec3c
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-05 05:04:14 +0000

    added admin tag

--- scripts/command/ctl.js
+++ scripts/command/ctl.js
@@ -1,3 +1,4 @@
+import Config from "../conf/Configuration";
 import { util } from "../util/utils";
 
 
@@ -5,9 +6,9 @@ import { util } from "../util/utils";
 export function ctl(msg) {
     msg.cancel = true;
     let player = msg.sender;
-    if (!player.hasTag('staffstatus') || !player.hasTag('Adminer')) {
+    if (!player.hasTag(Config.AdminTag)) {
         return util.sendMsgToPlayer(player, `§cComando desconocido: ctl. Revisa que el comando exista y que tengas permiso para usarlo.`);    }
-    else if (player.hasTag('staffstatus') && player.hasTag('Adminer')) {
+    else {
         util.sendMsgToPlayer(player, `§6Tp to control!`);
         util.sendMsg("@a[tag=Adminer]", `§c[!]§r ${player.nameTag}§a was tp to Control.`);
         player.runCommandAsync(`tp @s 50018 202 50006`)
