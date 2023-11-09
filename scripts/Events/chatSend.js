23e22621c2e10d62060fb2bf91140dea2b9952be
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-09 07:40:39 +0000

    se agrega nuevo sistema de filtro de comandos

--- scripts/Events/chatSend.js
+++ scripts/Events/chatSend.js
@@ -1,7 +1,7 @@
 import Config from "../conf/Configuration";
 import { ctl } from "../command/admin/ctl";
 import Server from "../server";
-import { getCooldown } from "../Modules/Tempo/Cooldown";
+import { getCooldown, setCooldown } from "../Modules/Tempo/Cooldown";
 
 const prefix = Server.getPrefix()
 const commandDefinitions = Object.setPrototypeOf({
@@ -18,56 +18,81 @@ const commandDefinitions = Object.setPrototypeOf({
 }, null)
 
 Server.world.beforeEvents.chatSend.subscribe((eventData) => {
-        let player = eventData.sender;
-        let msg = eventData.message
-        let tags = player.getTags()
-        let rank
-        //Evita que los jugadores muteados hablen en el chat
-        if (player.isMuted()) {
-            Server.sendMsgToPlayer(player, `${Config.serverName}§c Has sido silenciad@.`)
-            eventData.cancel = true
-            return;
+    let player = eventData.sender;
+    let msg = eventData.message
+    let tags = player.getTags()
+    let rank
+    //Evita que los jugadores muteados hablen en el chat
+    if (player.isMuted()) {
+        Server.sendMsgToPlayer(player, `${Config.serverName}§c Has sido silenciad@ en el chat global.`)
+        eventData.cancel = true
+        return;
+    }
+
+    if (msg.startsWith(prefix)) {
+        eventData.cancel = true;
+        let args = msg.slice(prefix.length).trim().split(/ +/)
+        const commandCall = args.shift().toLowerCase()
+        let cmd = Server.Commands.getRegistration(commandCall)
+        if (getCooldown("command", player) > 0) return Server.sendMsgToPlayer(player, `Porfavor espera, el comando esta en cooldown por §e${getCooldown("command", player)}s!`)
+        if (!cmd) {
+            Server.sendMsgToPlayer(player, `§cComando desconocido: ${commandCall}. Revisa que el comando exista y que tengas permiso para usarlo.`)
+            return eventData.cancel = true
         }
 
-        if (msg.startsWith(prefix)) {
-            eventData.cancel = true;
-            let arg = msg.slice(prefix.length).trim().split(/ +/)
-            const commandName = arg.shift().toLowerCase()
-            //let cmd = Server.C
-            //Registro de LogScreen
-            if (Config.debug) {
-                //console.warn(`${new Date()} | did run command handler`)
+        //Advierte que el comando no es parte del CommandDefinition Handler
+        //if (!(commandCall in commandDefinitions)) {
+        //    Server.sendMsgToPlayer(player, `§cComando desconocido: ${commandCall}. Revisa que el comando exista y que tengas permiso para usarlo.`)
+        //    return eventData.cancel = true
+        //}
 
-            }
-            if(getCooldown("command", player) > 0) return Server.sendMsgToPlayer(player, `Porfavor espera, el comando esta en cooldown por §e${getCooldown("command", player)}s!`)
-
-            //if (!cmd)
-
-            //Advierte que el comando no es parte del CommandDefinition Handler
-            if (!(commandName in commandDefinitions)) {
-                Server.sendMsgToPlayer(player, `§cComando desconocido: ${commandName}. Revisa que el comando exista y que tengas permiso para usarlo.`)
-                return eventData.cancel = true
-            }
-            //Ejecuta los comandos que coincidan en el CommandDefinition handler
-            commandDefinitions[commandName](eventData, arg, msg.slice(prefix.length + commandName.length + 1), commandName)
-            console.warn(`${new Date()} | "${player.name}" used the command: ${prefix}${commandName}  ${arg} and ${arg.join(" ")}`)
-
+        /**
+         * recuerda registrar el comando en {Settings}
+         * esto verifica que el comando no seaa de la categoria System o si esta desabilitado
+         */
+        if ((Server.Setting.get(`${cmd.category.toLowerCase()}System`) ?? true) == false) {
+            Server.sendMsgToPlayer(player, `§cComando desconocido: ${commandCall}. Revisa que el comando exista y que tengas permiso para usarlo.`)
+            return eventData.cancel = true
         }
+        //Se ejecuta si el comando no esta registrado en Settings y en Configuracion aparece como falso o undefined 
+        if ((Server.Setting.get(`${cmd.settingname}System`) ?? Config.Commands[cmd.category.toLowerCase()][cmd.settingname]) == false) {
+            Server.sendMsgToPlayer(player, `§cComando desconocido: ${commandCall}. Revisa que el comando exista y que tengas permiso para usarlo.`)
+            return eventData.cancel = true
+        }
+        //valida que el comando no sea de clase Admin
+        if (cmd.admin && !player.hasTag(Config.AdminTag)) {
+            Server.sendMsgToPlayer(player, `§cComando desconocido: ${commandCall}. Revisa que el comando exista y que tengas permiso para usarlo.`)
+            return eventData.cancel = true
+        }
+        //Ejecuta el comando
+        Server.System.run(() => {
+            try {
+                cmd.callback(data, player, args)
+            } catch (err) { console.warn(err) }
+        })
+        //ejecuta el cooldown para los comandos
+        setCooldown("command", player, Server.Setting.get("commandCooldown") ?? Config.commandCooldown)
+        Log(`[Command] ${player.name} used ${cmd.name} command.`)
+        //Ejecuta los comandos que coincidan en el CommandDefinition handler
+        //commandDefinitions[commandCall](eventData, args, msg.slice(prefix.length + commandCall.length + 1), commandCall)
+        //console.warn(`${new Date()} | "${player.name}" used the command: ${prefix}${commandCall}  ${args} and ${args.join(" ")}`)
 
-        for (const tag of tags) {
-            if (tag.startsWith("Rank:")) {
-                rank = tag.replace("Rank:", "")
-                rank = rank.replaceAll("--", "§r§o§7][§r")
-            }
+    }
+    //Se asigna rango
+    for (const tag of tags) {
+        if (tag.startsWith("Rank:")) {
+            rank = tag.replace("Rank:", "")
+            rank = rank.replaceAll("--", "§r§o§7][§r")
         }
-        if (!rank) {
-            rank = "★";
-        }
-        //envia el mensaje al chat general
-        if (!eventData.cancel) {
-            Server.sendMsgAll(
-                "@a", `§r§o§7${player.name}§7 [§8${rank}§r§o§7] >> §r${msg}`
-            )
-            eventData.cancel = true;
-        }
-    })
+    }
+    if (!rank) {
+        rank = "★";
+    }
+    //envia el mensaje al chat general
+    if (!eventData.cancel) {
+        Server.sendMsgAll(
+            "@a", `§r§o§7${player.name}§7 [§8${rank}§r§o§7] >> §r${msg}`
+        )
+        eventData.cancel = true;
+    }
+})
