7893d87fa1784316fe787792dcd16fd8e735350c
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-12 13:06:54 -0600

    Added comand cartera

---
+++ scripts/Commands/money/cartera.js
@@ -0,0 +1,25 @@
+import Server from "../../server";
+import Utility from "../../Modules/Utilities/Utility"
+
+Server.Commands.register({
+    name: "cartera",
+    description: "Checa tu dinero o el de alguien más",
+    usage: "cartera <nombre de jugador?>",
+    aliases: ["cash", "balance", "money"],
+    category: "Money"
+}, async (data, player, args) => {
+    if (!args[0]) {
+        let playerMoney = player.getMoney()
+        Server.sendMsgToPlayer(player, `§6Tu Balance es: §e${Utility.formatMoney(playerMoney)}`)
+    } else {
+        let extractData = await Utility.ExtractNameFromString(args.join(" "), 0)
+        let targetPlayer = await Server.getPlayer(extractData?.name ?? "")
+        if (targetPlayer != undefined) {
+            let targetMoney = targetPlayer.getMoney()
+            Server.sendMsgToPlayer(player, `§6El Balance de §e${targetPlayer.name}§6 es: §e${Utility.formatMoney(targetMoney)}`)
+        } else {
+            let playerMoney = player.getMoney()
+            Server.sendMsgToPlayer(player, `§6Tu Balance es: §e${Utility.formatMoney(playerMoney)}`)
+        }
+    }
+})
