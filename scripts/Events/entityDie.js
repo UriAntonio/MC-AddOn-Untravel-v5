575c77d34c0912b1ecc1f9add8840ce6650c54b8
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-12 19:08:34 -0600

    working earnMoney from mobs

---
+++ scripts/Events/entityDie.js
@@ -0,0 +1,18 @@
+import Config from "../conf/Configuration"
+import Server from "../server"
+import Utility from "../Modules/Utilities/Utility"
+
+Server.world.afterEvents.entityDie.subscribe( async data => {
+    if (!(Server.Setting.get("earnMoneyfromMobs") ?? Config.earnMoneyfromMobs)) return
+    if (data.damageSource.cause == "none") return
+    if (data.damageSource.damagingEntity == undefined || data.damageSource.damagingEntity.typeId != "minecraft:player") return
+    let deadEntity = data.deadEntity
+    let player = data.damageSource.damagingEntity
+    
+    let entity = Config.moneyFromMobs[deadEntity.typeId]
+    if (!entity) return
+    let moneyEarn = Utility.random(entity[0], entity[1])
+    if (moneyEarn <= 0) return
+    await player.setMoney(player.getMoney() + moneyEarn)
+    Server.System.run(() => player.onScreenDisplay.setActionBar(`§gObtiviste §e${Utility.formatMoney(moneyEarn)}`))
+})
