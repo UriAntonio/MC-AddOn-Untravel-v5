2ef0d846683030081e41aee3837b02885f37b200
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-14 22:00:43 -0600

    Added before event system and log

--- scripts/Events/playerLeave.js
+++ scripts/Events/playerLeave.js
@@ -1,8 +1,17 @@
+import { Player } from "@minecraft/server"
 import { Log } from "../Modules/Log/Log"
 import Server from "../server"
 
+/**
+ * @param {Player} data
+ */
+
 Server.Minecraft.world.afterEvents.playerLeave.subscribe((data) => {
-  let player = data.playerName
-  Log(`§e${data.playerName}§r se desconecto del servidor ${Date()}`)
-  Server.PlayerOnline[player] = undefined
+  let playerN = data.playerName
+  Server.PlayerOnline[playerN] = undefined
+})
+
+Server.world.beforeEvents.playerLeave.subscribe(async (data) => {
+  let player = data.player
+  Log(`§e${player.name}§a se desconecto del servidor ${Date()} en §2${Math.floor(player.location.x)}, ${Math.floor(player.location.y)}, ${Math.floor(player.location.z)}`)
 })
