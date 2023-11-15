7055ac536329c845e43e0ee83948d591ce24278a
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-14 23:50:20 -0600

    added info

--- scripts/Extensions/Gamemode.js
+++ scripts/Extensions/Gamemode.js
@@ -2,6 +2,11 @@
 // https://github.com/TrippleAWap
 import { Player } from '@minecraft/server';
 
+/**
+ * @name gamemode
+ * @returns {get}  retorna el modo de juego em string
+ * @param {set} string asigana el modo de juego
+ */
 Object.defineProperty(Player.prototype, "gamemode", {
     get() {
         return ["survival", "creative", "adventure", "spectator"].find(i => this.matches({ gameMode: i }))
