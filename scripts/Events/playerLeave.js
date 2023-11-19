38ba1307c71e7851518510d406ccf08a5762e580
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-18 22:51:22 -0600

    added game mode log when playerleave

--- scripts/Events/playerLeave.js
+++ scripts/Events/playerLeave.js
@@ -5,13 +5,16 @@ import Server from "../server"
 /**
  * @param {Player} data
  */
-
 Server.Minecraft.world.afterEvents.playerLeave.subscribe((data) => {
   let playerN = data.playerName
   Server.PlayerOnline[playerN] = undefined
 })
 
+/**
+ * @param {Player} data
+ */
 Server.world.beforeEvents.playerLeave.subscribe(async (data) => {
   let player = data.player
-  Log(`§e${player.name}§a se desconecto del servidor ${Date()} en §2${Math.floor(player.location.x)}, ${Math.floor(player.location.y)}, ${Math.floor(player.location.z)}`)
+  let gm = player.gamemode
+  Log(`§e${player.name}§a se desconecto del servidor ${Date()} en §2${Math.floor(player.location.x)}, ${Math.floor(player.location.y)}, ${Math.floor(player.location.z)} | gm: ${gm}`)
 })
