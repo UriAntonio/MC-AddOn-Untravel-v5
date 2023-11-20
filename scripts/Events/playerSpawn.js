be3d460d92ce3759ba925e640ca2ebd9152c2adc
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-19 19:33:42 -0600

    fixed bug when gm is spectator

--- scripts/Events/playerSpawn.js
+++ scripts/Events/playerSpawn.js
@@ -5,6 +5,7 @@ import Server from "../server.js";
 import { Log } from "../Modules/Log/Log.js";
 import { Player } from "@minecraft/server";
 
+
 /**
  * 
  * @param {Player} player 
@@ -12,43 +13,49 @@ import { Player } from "@minecraft/server";
 function onJoinSpawn(player) {
   try {
     player.runCommandAsync(`testfor @s`);
+   
     // Bloquea el server si esta en true
     if (Config.lockServer) {
       let reason = "Bajo Mantenimiento! Perdón por las molestias.";
       try {
         // Kick a los jugadores del server
-        player.runCommand(`kick ${JSON.stringify(player.name)} ${reason}`);
+        player.runCommandAsync(`kick ${JSON.stringify(player.name)} ${reason}`);
       } catch (error) {
         // Despawn players from server
         player.triggerEvent("minecraft:kick");
       }
     }
-    //Verificamos si el jugador es miembro nuevo
-      const gamemode = player.gamemode
-      welcome(player, gamemode);
-      Log(`§e${player.name}§r se unió al servidor ${Date()} | gm:  ${gamemode}`)
+    let gamemode
+    player.runCommandAsync(`gamemode s @s[tag=!${Config.AdminTag}]`)
+    //Verificamos el modo de juego del jugador
+    gamemode = player.gamemode
+    if (!player.hasTag(Config.AdminTag)) {
+      gamemode = "survival"
+    }
+    welcome(player, gamemode);
+    Log(`§e${player.name}§r se unió al servidor ${Date()} | gm:  ${gamemode}`)
     // Ejecutanos cada comando en la lista
     for (let i = 0; i < onJoinData.length; i++) {
       try {
         player.runCommandAsync(`${onJoinData[i]}`);
-      } catch (error) {}
+      } catch (error) { }
     }
-  } catch (error) { Log(`§cError al unirse: ${player} | ${error}`)}
+  } catch (error) { Log(`§cError al unirse: ${player} | ${error}`) }
 }
 
-  Server.world.afterEvents.playerSpawn.subscribe((loaded) => {
-    // Toma el nombre del jugador que se esta uniendo
-    let player = loaded.player;
-    // Ejecuta la funcion con el tick event
-    if (loaded.initialSpawn) {
-      Server.PlayerOnline[player.name] = Date.now()
-      Server.System.run(async () => {
-        onJoinSpawn(player);
-      });
-      
-    } else {
-      //player.sendMessage(`§eYou died. use §a!back§e to teleport to your death location.`)
-    }
-    
-  });
+Server.world.afterEvents.playerSpawn.subscribe((loaded) => {
+  // Toma el nombre del jugador que se esta uniendo
+  let player = loaded.player;
+  // Ejecuta la funcion con el tick event
+  if (loaded.initialSpawn) {
+    Server.PlayerOnline[player.name] = Date.now()
+    Server.System.run(() => {
+      onJoinSpawn(player);
+    });
+
+  } else {
+    //player.sendMessage(`§eYou died. use §a!back§e to teleport to your death location.`)
+  }
+
+});
 
