d9a7b514cc9e874c5ac6f4d89e242e85cd931b6b
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-20 22:44:38 -0600

    Se agrego modulo de color

---
+++ scripts/util/colors.js
@@ -0,0 +1,113 @@
+/**
+ * Base de colores y codigos de color
+ */
+const c = {}
+ /**
+  * #000000 negro
+  */
+c.c0 = "§0"
+
+/**
+ * @return {string} #0000AA azul marino 
+ */
+c.c1 = "§1"
+
+/**
+ * #00AA00 verde oscuro
+ */
+c.c2 = "§2"
+
+/**
+ * #00AAAA axul prismarina
+ */
+c.c3 = "§3"
+
+/**
+ * #AA0000 rojo oscuro
+ */
+c.c4 = "§4"
+
+/**
+ * #AA00AA morado oscuro
+ */
+c.c5 = "§5"
+
+/**
+ * #FFAA00 amarillo mostaza
+ */
+c.c6 = "§6" 
+
+/**
+ * #AAAAAA gris
+ */
+c.c7 = "§7"
+
+/**
+ * #555555 gris oscuro
+ */
+c.c8 = "§8"
+
+/**
+ * #5555FF azul medio 
+ */
+c.c9 = "§9"
+
+/**
+ * #55FF55 verde limon
+ */
+c.ca = "§a"
+
+/**
+ * #55FFFF azul brillante
+ */
+c.cb = "§b"
+
+/**
+ * #FF5555 rojo brillante
+ */
+c.cc = "§c"
+
+/**
+ * #FF55FF morado brillante
+ */
+c.cd = "§d"
+
+/**
+ * #FFFF55 amarillo brillante
+ */
+c.ce = "§e"
+
+/**
+ * #FFFFFF blanco
+ */
+c.cf = "§f"
+
+/**
+ * #FFC300 amarillo oro
+
+ */
+c.cg = "§g"
+
+/**
+ * Formato Obfuscado
+ */
+c.ck = "§k"
+
+/**
+ * Formato letra Hegra(grueso) Bold
+ */
+c.cl = "§l"
+
+/**
+ * Formato Italica(cursiva)
+ */
+c.co = "§o"
+
+/**
+ * Reseteo de Formato
+ */
+c.cr = "§r"
+
+
+
+export default c
