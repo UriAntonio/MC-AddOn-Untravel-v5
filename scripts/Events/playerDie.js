ec67ab61491d3955340bdda9189a151f04739e47
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-21 23:58:47 -0600

    added a switch for back data

--- scripts/Events/playerDie.js
+++ scripts/Events/playerDie.js
@@ -3,6 +3,7 @@ import Server from "../server"
 Server.Minecraft.world.afterEvents.entityDie.subscribe((data) => {
   if (data.deadEntity.isValid() && data.deadEntity.typeId === "minecraft:player") {
     let player = data.deadEntity
+    if ((Server.Setting.get("backSystem") ?? true) == false) return player.sendMessage(`§a■§3Moriste en: §c${Math.floor(player.location.x)}, ${Math.floor(player.location.y)}, ${Math.floor(player.location.z)}`);
     const backData = {
       x: Math.floor(player.location.x),
       y: Math.floor(player.location.y),
