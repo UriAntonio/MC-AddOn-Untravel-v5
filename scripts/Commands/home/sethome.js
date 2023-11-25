b3b91488dbe30aa4f50a81d2a4d018c9a3f9f469
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-25 12:06:15 -0600

    added cost to sethome

--- scripts/Commands/home/sethome.js
+++ scripts/Commands/home/sethome.js
@@ -1,3 +1,4 @@
+import Money from "../../Modules/Finance/Money"
 import Config from "../../conf/Configuration"
 import Server from "../../server"
 
@@ -6,23 +7,39 @@ const HomeDB = Server.HomeDB
 Server.Commands.register({
   name: "sethome",
   aliases: ["sh"],
-  description: "Agrega un hogar con el nombre seleccionado",
+  description: `Agrega un hogar con el nombre seleccionado, Costo inicial(se duplica por cada hogar): §f${Config.homeCost} `,
   usage: "sethome <home_name>",
   category: "Home"
 }, async (data, player, args) => {
+  let homeArr = []
+  let cost = Config.homeCost
+  HomeDB.forEach((key) => {
+    if (key.startsWith(player.name)) {
+      let hname = key.substring(`${player.name}-`.length)
+      homeArr.push(`${hname}`);
+    }
+  })
+  for (let i = 0; i < homeArr.length; i++) {
+    cost = cost + cost;
+    
+  }
+  
+  let balance = Money.getMoney(player.name)
   if (!args[0]) return player.sendMessage("§a■§cIngresa un mombre para tu hogar.")
   let homeCount = HomeDB.keys().filter(t => t.startsWith(player.name)).length
   if (!player.hasTag(Config.AdminTag) && homeCount >= (Server.Setting.get("homeLimit") ?? Config.homeLimit))
-   return player.sendMessage("§a■§cYa tienes el Maximo de hogares!")
+    return player.sendMessage("§a■§cYa tienes el Maximo de hogares!")
   let name = args.slice(0).join(" ")
   let playerHome = HomeDB.keys().find(key => key == `${player.name}-${name}`)
   if (playerHome != undefined) return player.sendMessage("§a■§cNombre de hogar duplicado, hogar no fue creado.")
+  if (balance < cost) return player.sendMessage(`§a■§cNo cuentas con fondos suficiente. Costo: §f${cost}`)
   const homeObject = {
     x: player.location.x,
     y: player.location.y,
     z: player.location.z,
     dimension: player.dimension.id
   }
+  Money.setMoney(player, balance - cost)
   await HomeDB.set(`${player.name}-${name}`, homeObject)
   player.sendMessage(`§1------------------------------\n§a■§3Home creada exitosamente con el nombre §f${name}§3!`)
 })
