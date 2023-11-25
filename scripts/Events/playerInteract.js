1a379a102065b65f68c544d01729162f81962da4
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-24 23:13:14 -0600

    added some word

--- scripts/Events/playerInteract.js
+++ scripts/Events/playerInteract.js
@@ -3,7 +3,7 @@ import Server from "../server";
 Server.world.beforeEvents.playerInteractWithBlock.subscribe((event) => {
     const { block, itemStack, player } = event;
     if ( block.typeId === "minecraft:chest" && player.hasTag("lockChest")) {
-        player.sendMessage(`§a■§cNo tienes permisos`)
+        player.sendMessage(`§a■§cNo tienes permisos aqui`)
             event.cancel = true;
     }
   })
