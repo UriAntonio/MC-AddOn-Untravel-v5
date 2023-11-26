8c42e3a1cf6c3e7942fa40d2e51195f07f484e22
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-25 21:43:43 -0600

    added Dynamicproperties database

---
+++ scripts/Extensions/Database.js
@@ -0,0 +1,51 @@
+/**
+ * @author This database module was created by THE BOSS9345 for use with Minecraft Bedrock Api.
+ */
+
+import { world, Player } from "@minecraft/server";
+
+Object.defineProperty(globalThis, 'Database', {
+    get: function () {
+        return {
+            /**
+             * Set a value in the database.
+             * @param {string} key - The database key.
+             * @param {boolean | number | string} value - The value to set.
+             * @param {Player} [player=null] - The player for whom to set the database (default: null, i.e., set to world).
+             * @example
+             * set('auction', JSON.stringify({'dirt': 1})) // set to world
+             * set('auction', JSON.stringify({'dirt': 1}), player) // set to player
+             */
+            set(key, value, player = null) {
+                (player || world).setDynamicProperty(key, value);
+            },
+              /**
+             * Get a value from the database.
+             * @param {string} key - The database key.
+             * @param {Player} [player=null] - The player from whom to get the database (default: null, i.e., get from world).
+             * @returns {boolean | number | string | null} The value of the specified key.
+             */
+            get(key, player = null) {
+                return (player || world).getDynamicProperty(key);
+            },
+             /**
+             * Check if a key exists in the database.
+             * @param {string} key - The database key.
+             * @param {Player} [player=null] - The player to check for the key (default: null, i.e., check in world).
+             * @returns {boolean} True if the key exists, false otherwise.
+             */
+            has(key, player = null) {
+                return !!(player || world).getDynamicProperty(key);
+            },
+            /**
+             * Delete a key from the database.
+             * @param {string} key - The database key to delete.
+             * @param {Player} [player=null] - The player from whom to delete the key (default: null, i.e., delete from world).
+             */
+            delete(key, player = null) {
+                (player || world).setDynamicProperty(key, null);
+            }
+        };
+    }   
+});
+export default globalThis.Database;
