b448222689a2eeb729a5de3675624af8d45f8633
KlarixMx <antoniotorres.uri@gmail.com>
2023-12-01 00:17:06 -0600

    added validators to coliseo

--- scripts/Events/playerInteract.js
+++ scripts/Events/playerInteract.js
@@ -1,5 +1,6 @@
 import Server from "../server";
 import { coliseoCost } from "../Estructuras/Coliseo/coliseoManager";
+import { getWorldCooldown } from "../Modules/Tempo/Cooldown";
 
 
 
@@ -15,13 +16,15 @@ Server.world.beforeEvents.playerInteractWithBlock.subscribe((event) => {
     if (block.typeId === "minecraft:chemistry_table") {
         event.cancel = true;
         if (Database.get("coliseoManager") == true) {
-
+            if (getWorldCooldown("coliseo", Database.get("coliseoCooldown")) > 0) return player.sendMessage(`§a■§cEl Coliseo Ya esta en uso, espera: ${getWorldCooldown("coliseo", Database.get("coliseoCooldown"))}s.`)
+            //if (player.isPunish) return player.sendMessage("§a■§cNo puedes usar el Coliseo pues estas Castigado")
+            //if (player.isAdmin()) return Coliseo(player)
             coliseoCost(player)
         } else {
             player.sendMessage("§a■§cEl Coliseo esta desactivado")
         }
-        
-        
+
+
     }
 }
 )
