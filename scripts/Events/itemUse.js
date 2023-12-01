33dfb0a9558215110de22529ca6bdf4b1ef8420b
KlarixMx <antoniotorres.uri@gmail.com>
2023-11-30 23:02:37 -0600

    added system run

--- scripts/Events/itemUse.js
+++ scripts/Events/itemUse.js
@@ -10,23 +10,26 @@ import { Player } from "@minecraft/server";
  * @returns 
  */
 function MsgLogs(player) {
-let logDataMsg = [...LogDataMsg].reverse()
+    let logDataMsg = [...LogDataMsg].reverse()
     logDataMsg.splice(0, 1)
-    const logForm = new MessageFormData()
-        .title("§9§l Msg Logs")
-        .body(logDataMsg.join("\n§r"))
-        .button2('§l§bLimpiar')
-        .button1('§l§1Cerrar')
+    Server.System.run(() => {
+        const logForm = new MessageFormData()
+            .title("§9§l Msg Logs")
+            .body(logDataMsg.join("\n§r"))
+            .button2('§l§bLimpiar')
+            .button1('§l§1Cerrar')
 
-    player.sendMessage("§1------------------------------\n§a■§3Cierra el Chat para ver el Panel")
-    let res = ForceOpen(player, logForm)
-    if(!res.canceled) {
-        if (res.selection == 1) {
-            //ClearLog()
-            //Log(`[Logs] ${player.name}  Msg Logs limpiados`)
-            return player.sendMessage("§1------------------------------\n§a■§3Se limpiaron los Logs correctamente")
-          }
-    }
+        player.sendMessage("§1------------------------------\n§a■§3Cierra el Chat para ver el Panel")
+        let res = ForceOpen(player, logForm)
+        if (!res.canceled) {
+            if (res.selection == 1) {
+                //ClearLog()
+                //Log(`[Logs] ${player.name}  Msg Logs limpiados`)
+                return player.sendMessage("§1------------------------------\n§a■§3Se limpiaron los Logs correctamente")
+            }
+        }
+    })
+
 }
 
 Server.world.beforeEvents.itemUse.subscribe((data) => {
@@ -34,6 +37,6 @@ Server.world.beforeEvents.itemUse.subscribe((data) => {
     const items = data.itemStack
     if (items.typeId == "mx:new" &&
         source.isAdmin()) {
-       MsgLogs(source)
+        MsgLogs(source)
     }
 })
