04b6fc8ab3a9e32a9b4564ad640f27f8d6e34eff
KlarixMx <antoniotorres.uri@gmail.com>
2023-12-03 21:45:52 -0600

    added comand allhomes for admins

---
+++ scripts/Commands/admin/allhomes.js
@@ -0,0 +1,28 @@
+import Utility from "../../Modules/Utilities/Utility"
+import Server from "../../server"
+
+const HomeDB = Server.HomeDB
+
+Server.Commands.register({
+    name: "allhomes",
+    aliases: ["ah"],
+    description: "ve lista completa de homes guardados",
+    usage: "allhomes",
+    admin: true,
+    category: "Admin"
+}, async (data, player, args) => {
+    let message = ""
+    HomeDB.forEach((key, value) => {
+        if (key) {
+          let home = value
+          let hname = key
+          message += `\n§1  |§f ${hname}§1  |§b ${Math.round(home.x)}, ${Math.round(home.y)}, ${Math.round(home.z)} §1| §9${home.dimension}`;
+        }
+      })
+      if (message != "") {
+        player.sendMessage(`§1------------------------------\n§a■§3Homes List : ${message}`)
+      }
+      else {
+        player.sendMessage("§a■§cHo hay Hogares.")
+      }
+})
