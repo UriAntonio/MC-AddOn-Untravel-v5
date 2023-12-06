9086a30a73008d3b5368c64d0145556d49c0d509
KlarixMx <antoniotorres.uri@gmail.com>
2023-12-05 22:06:50 -0600

    added a temporal restriction

--- scripts/Events/playerInteract.js
+++ scripts/Events/playerInteract.js
@@ -16,6 +16,7 @@ Server.world.beforeEvents.playerInteractWithBlock.subscribe((event) => {
     if (block.typeId === "minecraft:chemistry_table") {
         event.cancel = true;
         if (Database.get("coliseoManager") == true) {
+            if (!player.isAdmin()) return
             if (getWorldCooldown("coliseo", Database.get("coliseoCooldown")) > 0) return player.sendMessage(`§a■§cEl Coliseo Ya esta en uso, espera: ${getWorldCooldown("coliseo", Database.get("coliseoCooldown"))}s.`)
             //if (player.isPunish) return player.sendMessage("§a■§cNo puedes usar el Coliseo pues estas Castigado")
             //if (player.isAdmin()) return Coliseo(player)
