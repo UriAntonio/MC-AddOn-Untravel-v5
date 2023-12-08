18116cbba73063b85d6f26aa84dbdaefac2389fe
KlarixMx <antoniotorres.uri@gmail.com>
2023-12-07 21:52:21 -0600

    Added AllMoney command to admin

---
+++ scripts/Commands/admin/allmoney.js
@@ -0,0 +1,25 @@
+import Utility from "../../Modules/Utilities/Utility"
+import Server from "../../server"
+
+
+Server.Commands.register({
+  name: "allmoney",
+  aliases: ["am"],
+  description: "Mira todos lo que tienen Dinero",
+  usage: "allmoney",
+  admin: true,
+  category: "Admin"
+}, async (data, player, args) => {
+  let BalanceData = Server.Money.getAllMoney()
+  if (BalanceData.length <= 0) return player.sendMessage("§1------------------------------\n§a■§3No hay jugadores con fondos.")
+  BalanceData.sort((a, b) => b.playerMoney - a.playerMoney)
+  let show =  BalanceData.length
+  if (BalanceData.length < 10) show = BalanceData.length
+  let message = "§1------------------------------\n§a■§1[§9TABLA DE DINERO§1]"
+  for (let i = 0; i < show; i++) {
+    let playerName = BalanceData[i].playerName
+    let playerMoney = Number(BalanceData[i].playerMoney)
+    message += `\n§9${i + 1}. §3${playerName}: §b${Utility.formatMoney(playerMoney)}`
+  }
+  player.sendMessage(message)
+})
