876fae141f7b98e6e34018af3441e4980b70a6bc
KlarixMx <antoniotorres.uri@gmail.com>
2023-12-15 10:42:15 -0600

    transform format of for each

--- scripts/Commands/general/time.js
+++ scripts/Commands/general/time.js
@@ -1,6 +1,7 @@
 import { ActionFormData } from "@minecraft/server-ui";
 import Untravel from "../../Untravel";
 import { ForceOpen } from "../../Modules/Server/Forms";
+import { LogWarn } from "../../Modules/Log/Log";
 
 const TimeDB = Untravel.TimeDB
 
@@ -12,18 +13,20 @@ Untravel.Commands.register({
 }, async (data, player, args) => {
     let title ="§a■§1[§9TIEMPO DE JUGADORES§1]§a■"
     let message = ""
-    Untravel.Minecraft.world.getAllPlayers().forEach(plr => {
+    LogWarn(`${JSON.stringify(TimeDB.entries())}`)
+    TimeDB.forEach((key, value) => {
+      let plr = key
       const DateNow = new Date()
-      const DateLogin = TimeDB.get(plr.name)
+      const DateLogin = value//TimeDB.get(plr)
       const SecondPlayed = Math.ceil((DateNow - DateLogin) / 1000);
-      message += `\n§1 | §3${plr.name} §1| §bEn Linea desde \n§1 -`
+      message += `\n§1 | §3${plr} §1| §bOnline desde§1 -`
       if (SecondPlayed >= 86400) {
         let day = Math.floor(SecondPlayed / 86400)
         message += ` §f${day} §bdias,`
       }
       if (SecondPlayed >= 3600) {
         let hour = Math.floor(SecondPlayed / 3600)
-        message += ` §f${hour % 24} §bhoras,`
+        message += `\n§1 - §f${hour % 24} §bhoras,`
       }
       if (SecondPlayed >= 60) {
         let minute = Math.floor(SecondPlayed / 60)
@@ -36,7 +39,7 @@ Untravel.Commands.register({
     player.sendMessage("§1------------------------------\n§a■§3Cierra el Chat para ver el Panel")
     let res = await ForceOpen(player, form)
     if (!res.canceled){
-        player.sendMessage("no se cancelo")
+        player.playSound("random.levelup")
     }
 
     //player.sendMessage(message)
