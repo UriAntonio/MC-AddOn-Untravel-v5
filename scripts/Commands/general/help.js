2b60fd47fb0b7f7494a7408d8bd481e602afd17c
KlarixMx <antoniotorres.uri@gmail.com>
2023-12-19 22:24:47 -0600

    used method isOp

--- scripts/Commands/general/help.js
+++ scripts/Commands/general/help.js
@@ -17,7 +17,7 @@ Untravel.Commands.register({
     let helpMessage = `§1------------------------------\n§a■§6Help Mensage ("§e?§6" Esto significa que es opcional o podria no ser llenado)`
     for (const category of commandCategory) {
       if (category == "Admin" && !player.isAdmin()) continue;
-      if (category == "Op" && !player.hasTag("Adminer")) continue;
+      if (category == "Op" && !player.isOp()) continue;
       if ((Untravel.Setting.get(`${category.toLowerCase()}System`) ?? true) == false) continue;
       let commands = commandList.filter(c => c.category == category)
       helpMessage += `\n §9Comandos ${category} :`
