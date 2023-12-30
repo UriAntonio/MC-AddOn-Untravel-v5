e5467289e0f37dabc7f048ff0137978d9e2766fc
KlarixMx <antoniotorres.uri@gmail.com>
2023-12-30 00:12:35 -0600

    modify command Warp to ActionFormData

--- scripts/Commands/warp/warp.js
+++ scripts/Commands/warp/warp.js
@@ -4,26 +4,40 @@ import { getCooldown, setCooldown } from "../../Modules/Tempo/Cooldown"
 import Action from "../../Modules/Log/ActionLog"
 import Fund from "../../Modules/Finance/Funds"
 import Money from "../../Modules/Finance/Money"
+import { ActionFormData } from "@minecraft/server-ui"
+import untravel from "../../Extensions/untravel"
+import { ForceOpen } from "../../Modules/Server/Forms"
 
 const WarpDB = Untravel.WarpDB
-
+const { symbols: { portal, coin } } = untravel
 Untravel.cmd.add({
   name: "warp",
   description: "Teleport al warp seleccionado",
-  usage: "warp <nombre_de_lugar>",
+  usage: "warp",
   aliases: ["w"],
   category: "Warp"
 }, async (data, player, args) => {
+  let title = Config.serverTitle(`WARPS`)
+  let message = ""
   let cost = Config.warpCost
   let balanceFund = Fund.getMoney()
   let balance = Money.getMoney(player.name)
-  if (!args[0]) return player.sendMessage("§a■§cIngresa el mombre del Warp.")
-  let name = args.slice(0).join(" ")
-  let warp = WarpDB.get(name)
-  if (warp != undefined) {
-    if (name.toLowerCase() == "coliseo") cost = Config.warpColiseo
-    if (player.isCombat()) return player.sendMessage("§a■§cEstas en Combate!")
-    if (balance < cost) return player.sendMessage(`§a■§cNo cuentas con fondos suficiente. Costo: §f${cost}`)
+  const warpsArr = WarpDB.lenght > 0 ? WarpDB.keys() : ["Sin Datos"]
+  if (player.isCombat()) return player.sendMessage("§a■§cEstas en Combate!")
+  const form = new ActionFormData().title(`${title}`).body(`${message}`)
+  warpsArr.forEach(x => {
+    let precio = x != "coliseo" ? Config.warpCost : Config.warpColiseo
+    form.button(`${portal} §l§d${x} ${coin}: §6${precio}`)
+  })
+  player.sendMessage(`${Config.FormMessage}`)
+  let res = await ForceOpen(player, form)
+  if (!res.canceled) {
+    let result = res.selection
+    let selected = warpsArr[result]
+    if (selected == "Sin Datos") return player.sendMessage(`${Config.serverStyler}§cNo hay Warps agregados`)
+    if (selected == "coliseo") cost = Config.warpColiseo
+    if (balance < cost && !player.isAdmin()) return player.sendMessage(`§a■§cNo cuentas con fondos suficiente. Costo: §f${cost}`)
+    const warp = WarpDB.get(selected)
     if (getCooldown("warp", player) > 0) return player.sendMessage(`§a■§cYa has usado el comando home! En enfriamiento por: §f${getCooldown("warp", player)}s.`)
     let warpCD = Untravel.Setting.get("warpCooldown") ?? Config.warpCooldown
     setCooldown("warp", player, warpCD)
@@ -37,7 +51,7 @@ Untravel.cmd.add({
       for (let i = 0; i < warpCountdown; i++) {
         if (player.isCombat() || player.location.x != playerPosition.x || player.location.y != playerPosition.y || player.location.z != playerPosition.z) cancel = true
         if (cancel) {
-          if (!canceled) player.sendMessage("§1------------------------------\n§a■§cCancelado!")
+          if (!canceled) player.sendMessage(`${Config.serverStyler}§cCancelado!`)
           canceled = true
           return;
         }
@@ -51,8 +65,12 @@ Untravel.cmd.add({
     }
     player.sendMessage("§a■§3Teletransportando...")
     await Untravel.teleportPlayer(player, warp, { dimension: Untravel.getDimension(warp.dimension) })
-    player.sendMessage("§1------------------------------\n§a■§3Teletransportado Correctamente.")
-  } else {
-    player.sendMessage("§a■§cWarp Invalido.")
+    player.sendMessage(`${Config.serverStyler}§3Teletransportado Correctamente.`)
+  
   }
+
+  
+  
+  
+  
 })
