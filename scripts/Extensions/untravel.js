b5df2189d1556af8da0c7b4644a377d6ebedc946
KlarixMx <antoniotorres.uri@gmail.com>
2024-01-03 20:33:10 -0600

    added more glyphs

--- scripts/Extensions/untravel.js
+++ scripts/Extensions/untravel.js
@@ -2,11 +2,18 @@ const {fromCodePoint} = String
 
 const untravel = {
     symbols: {
+        coin: fromCodePoint(0xE102),
         heart: fromCodePoint(0xE300),
         mana: fromCodePoint(0xE301),
         Chalenger : fromCodePoint(0xE302),
         Bronze: fromCodePoint(0xE307),
-        trash: fromCodePoint(0xE308)
+        trash: fromCodePoint(0xE308),
+        myPoint: fromCodePoint(0xE30E),
+        portal: fromCodePoint(0xE380),
+        whiteConfig: fromCodePoint(0xE387),
+        equis: fromCodePoint(0xE391),
+        less: fromCodePoint(0xE39A),
+        plus: fromCodePoint(0xE39C),
     },
     standar: {
         actualHealth: 20,
