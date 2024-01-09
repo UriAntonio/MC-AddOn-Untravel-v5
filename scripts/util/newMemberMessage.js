bab4a99d14057d30b21ab8e2eade7feeeacace34
KlarixMx <antoniotorres.uri@gmail.com>
2024-01-09 17:20:55 -0600

    edites strings msg in New member ui

--- scripts/util/newMemberMessage.js
+++ scripts/util/newMemberMessage.js
@@ -1,6 +1,7 @@
 import { Player } from "@minecraft/server";
 import { ActionFormData, ModalFormData } from "@minecraft/server-ui";
 import Untravel from "../Untravel";
+import Config from "../conf/Configuration";
 //import { Log } from "../Modules/Log/Log";
 
 const versionList = [
@@ -15,18 +16,10 @@ const Updates = {
   0: function updateAnuncio_0(playerid, gamemode) {
     Untravel.System.run(() => {
       let upd = new ActionFormData()
-        .title(`§2§lUpdate`)
-        .body(`§rActualizacion §l§gV.1.0.0 §6Finance§r`
-          + `\n     Saludos soy §l§eKlarixMx§r me alegra que sigas aquí. `
-          + `\nEn esta actualizacion te traigo:`
-          + `\n§l§2Comandos Personalizados§r`
-          + `\n   §6-cash (-cartera, -money -balance)§r`
-          + `\n     Este comando te muetra tu dinero actual`
-          + `\n§l§2Recompensas por Farmeo§r   `
-          + `\n      Ahora los mobs te dan cierta cantidad de dinero de forma aleatoria entre mas peligroso mas dinero`
-        )
+        .title(`§2Update`)
+        .body(`§fActualizacion §l§gV.1.0.0 §6Finance§r§f\nSaludos soy §l§eKlarixMx§r§f gracias por tu tiempo.\nEn esta actualizacion te traigo:\n§l§2Comandos Personalizados§r§f\n   §6-cash (-cartera, -money -balance)§f\n     Este comando te muetra tu dinero actual\n§l§2Recompensas por Farmeo§r§f\nLos mobs te dan cierta cantidad de dinero de forma aleatoria entre mas peligroso mas dinero`)
         .button(`§a§lJuguemos!`)
-        .button(`§0§lRegresar!`)
+        .button(`§f§lRegresar!`)
       upd.show(playerid).then((result) => {
         if (result.canceled) {
           versiones(playerid, gamemode)
@@ -45,17 +38,17 @@ const Updates = {
   1: function updateAnuncio_1(playerid, gamemode) {
     Untravel.System.run(() => {
       let upd = new ActionFormData()
-        .title(`§2§lUpdate`)
-        .body(`§rActualizacion §l§gV.1.0.1 §6General§r`
-          + `\n     Saludos soy §l§eKlarixMx§r me alegra que sigas aquí. `
+        .title(`§2Update`)
+        .body(`§fActualizacion §l§gV.1.0.1 §6General7r§f`
+          + `\nSaludos soy §l§eKlarixMx§r§f\ngracias por tu tiempo. `
           + `\nEn esta actualizacion te traigo:`
-          + `\n§l§2Comandos Personalizados§r`
-          + `\n   §6-help (-h)§r`
-          + `\n     Con este puedes ver los detalles de cada comando`
-          + `\n   §6-tps §r`
-          + `\n      Checa los Ticks por segundo del Servidor`
-          + `\n   §6-playerlist (-playerson, -players, -pl)§r`
-          + `\n      Ve la lsita de jugadores y su tiempo de conexion actual`
+          + `\n§l§2Comandos Personalizados§r§f`
+          + `\n§6-help (-h)§f`
+          + `\nCon este puedes ver los detalles de cada comando`
+          + `\n§6-tps §f`
+          + `\nCheca los Ticks por segundo del Servidor`
+          + `\n§6-playerlist (-playerson, -players, -pl)§f`
+          + `\nVe la lsita de jugadores y su tiempo de conexion actual`
         )
         .button(`§a§lJuguemos!`)
         .button(`§0§lRegresar!`)
@@ -78,16 +71,16 @@ const Updates = {
   2: function updateAnuncio_2(playerid, gamemode) {
     Untravel.System.run(() => {
       let upd = new ActionFormData()
-        .title(`§2§lUpdate`)
-        .body(`§rActualizacion §l§gV.1.0.2 §6Home§r`
-          + `\n§l§2Comandos Personalizados§r`
-          + `\n   §6-home (-hm)§r`
+        .title(`§2Update`)
+        .body(`§fActualizacion §l§gV.1.0.2 §6Home§f`
+          + `\n§l§2Comandos Personalizados§f`
+          + `\n   §6-home (-hm)§f`
           + `\n      Comando para teletransportarte a tus hogares $$$poco`
-          + `\n   §6-sethome (-sh)§r`
+          + `\n   §6-sethome (-sh)§f`
           + `\n      Agrega un home con nombre personalizado $$$mucho`
-          + `\n   §6-delhome (-dhome, -dh)§r`
+          + `\n   §6-delhome (-dhome, -dh)§f`
           + `\n      borra o remueve un home $te rembolsa la midad del $$`
-          + `\n   §6-listhome (-homelist, -homes, -lh)§r`
+          + `\n   §6-listhome (-homelist, -homes, -lh)§f`
           + `\n      Muestra todos tus home`
         )
         .button(`§a§lJuguemos!`)
@@ -110,16 +103,16 @@ const Updates = {
   3: function updateAnuncio_3(playerid, gamemode) {
     Untravel.System.run(() => {
       let upd = new ActionFormData()
-        .title(`§2§lUpdate`)
-        .body(`§rActualizacion §l§gV.1.0.3 §6Finance§r`
-          + `\n§l§2Comandos Personalizados§r`
-          + `\n   §6-pay (-pagar, -p)§r`
+        .title(`§2§Update`)
+        .body(`§fActualizacion §l§gV.1.0.3 §6Finance§f`
+          + `\n§l§2Comandos Personalizados§f`
+          + `\n   §6-pay (-pagar, -p)§f`
           + `\n      Hacer transferencias o pagar a otro jugador`
-          + `\n   §6-topmoney (-topbal, -topcartera, -topbalance, -tm)§r`
+          + `\n   §6-topmoney (-topbal, -topcartera, -topbalance, -tm)§f`
           + `\n      Mira la table de posicion de dinero`
-          + `\n   §6-delhome (-dhome, -dh)§r`
+          + `\n   §6-delhome (-dhome, -dh)§f`
           + `\n      borra o remueve un home $te rembolsa la midad del $$`
-          + `\n   §6-listhome (-homelist, -homes, -lh)§r`
+          + `\n   §6-listhome (-homelist, -homes, -lh)§f`
           + `\n      Muestra todos tus home`
         )
         .button(`§a§lJuguemos!`)
@@ -149,7 +142,7 @@ function versiones(player, gamemode) {
   Untravel.System.run(() => {
     let num = -1
     let ver = new ActionFormData()
-      .title(`§5★━━━━━━━━<§o§lVersiones§r§5>━━━━━━━━★`)
+      .title(`§5§oVersiones`)
       .body(`   §dAqui puedes ver los cambios y agregados en cada version`)
     versionList.forEach(version => {
       ver.button(`§0§l${version}`)
@@ -185,16 +178,12 @@ export function welcome(playerid, gamemode) {
   const gm = gamemode
   Untravel.System.run(() => {
 
-    //Log(`§e${playerid.name}§r esta en  ${gm}`)
+    //Log(`§e${playerid.name}§f esta en  ${gm}`)
     let wel = new ActionFormData()
-      .title(`§5★━━━━━━━━<§o§lBienvenido§r§5>━━━━━━━━★`)
+      .title(`§5§oBienvenido`)
       .body(
-        `\n§rBienvenido de nuevo a §l§dUntravel§6Mx`
-        + `\n§5${playerid.name}§r.`
-        + `\nEsperamos y te la pases bien`
-        + ` cualquier error o sugerencia`
-        + ` acércate a nosotros en:`
-        + `\n    §bsupport@untravelmx.com`
+        `§fBienvenido de nuevo a §l§dUntravel§6Mx\n§5${playerid.name}§r§f.\nEsperamos y te la pases bien cualquier error o sugerencia acércate a nosotros en:`
+        + `\n§bsupport@untravelmx.com`
       )
       .button(`§d§lA Jugar!`)
       .button(`§l§eNuevo Contenido!`)
@@ -206,12 +195,12 @@ export function welcome(playerid, gamemode) {
       } else {
         if (result.selection == 1) {
           playerid.runCommandAsync(`playsound random.levelup @s`)
-          playerid.sendMessage(`§a■§3Bienvenido §b${playerid.name}`)
+          playerid.sendMessage(`${Config.serverStyler}§3Bienvenido §b${playerid.name}`)
           playerid.runCommandAsync(`playsound random.anvil_use @s`)
           versiones(playerid, gm)
         } else {
           playerid.runCommandAsync(`playsound random.levelup @s`)
-          playerid.sendMessage(`§a■§3Bienvenido §b${playerid.name}`)
+          playerid.sendMessage(`${Config.serverStyler}§3Bienvenido §b${playerid.name}`)
           playerid.runCommandAsync(`gamemode ${gm}`)
         }
       }
