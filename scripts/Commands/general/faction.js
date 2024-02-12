6e873dbd069e64484c333c3d54ae6f9e6cbe0ea9
KlarixMx <antoniotorres.uri@gmail.com>
2024-02-12 10:49:57 -0600

    adde test of command faction

---
+++ scripts/Commands/general/faction.js
@@ -0,0 +1,11 @@
+import Untravel from "../../Untravel";
+
+Untravel.cmd.add( {
+    name: "faction",
+    aliases: ["f", "faccion"],
+    description: "Shows the faccion UI and Settings",
+    category: "General",
+    usage: "faction \n faction create <nombre>",
+}, (data, player, args) => {
+    
+})
