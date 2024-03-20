748297bece5d3e3e3b7c19e4e191152cc17041b2
KlarixMx <antoniotorres.uri@gmail.com>
2024-03-20 12:37:27 -0600

    added welkome page

--- scripts/windows/welcome/w-page-1.js
+++ scripts/windows/welcome/w-page-1.js
@@ -2,6 +2,7 @@ import { Player } from "@minecraft/server";
 import { ActionFormData } from "@minecraft/server-ui";
 import Untravel from "../../Untravel";
 import Config from "../../Commands/Configuration";
+import { signos } from "./w-page-2";
 
 
 
@@ -17,10 +18,7 @@ export function guia(playerid, gamemode) {
       let wel = new ActionFormData()
         .title(`§5§oBienvenido`)
         .body(
-          `§fBienvenido a §l§dUntravel§6Mx\n§5${playerid.name}§r§f.\nEsperamos y te la pases bien. \nSiéntete como en casa \nSe parte o crea tu propia comunidad libremente\nCualquier error o sugerencia acércate a nosotros en:`
-          + `\n§bsupport@untravelmx.com`
-          + `\n§fNuestro discord es:`
-          +`\n§dhttps://discord.gg/yNZBAgAC`
+          `§fBienvenido a §l§dUntravel§6Mx\n§5${playerid.name}§r§f.\nEsperamos y te la pases bien. \nSiéntete como en casa \nSe parte o crea tu propia comunidad libremente`
         )
         .button(`§d§l>>`)
   
@@ -29,16 +27,39 @@ export function guia(playerid, gamemode) {
         if (result.canceled) {
           guia(playerid, gamemode)
         } else {
-          
-            playerid.playSound(`item.book.page_turn`)
-            playerid.runCommandAsync(`gamemode ${gamemode}`)
-            //playerid.playSound(`random.anvil_use`)
-            //guia2(playerid, gm)
+            signos(playerid, gm)
         }
       })
     })
   }
   
+
+  export function welcome(playerid, gamemode) {
+    const gm = gamemode
+    Untravel.System.run(() => {
+
+      let wel = new ActionFormData()
+        .title(`§5§oBienvenido`)
+        .body(
+          `§fBienvenido de nuevo a §l§dUntravel§6Mx\n§5${playerid.name}§r§f.`
+        )
+        .button(`§d§l>>`)
+        .button(`§e§lGUIA`)
+  
+      wel.show(playerid).then((result) => {
+        playerid.runCommandAsync(`gamemode spectator @s`)
+        if (result.canceled) {
+          welcome(playerid, gm)
+        } 
+        if (result.selection == 0) {
+            playerid.runCommandAsync(`gamemode ${gm}`)
+        }
+        if (result.selection == 0) {
+          guia(playerid, gm)
+        }
+      })
+    })
+  }
   
   
   
