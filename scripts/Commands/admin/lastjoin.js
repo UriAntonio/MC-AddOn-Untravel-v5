7d2c42d2dc9ad90b8f50d506ee8ffe2db2ac6075
KlarixMx <antoniotorres.uri@gmail.com>
2024-03-20 20:51:03 -0600

    modified disign in last join command

--- scripts/Commands/admin/lastjoin.js
+++ scripts/Commands/admin/lastjoin.js
@@ -14,13 +14,13 @@ Untravel.cmd.add({
     admin: true
 }, async (data, player, args) => {
   const LastDB = Untravel.LastDB
-    let title ="§a■§1[§9ULTIMA CONEXION§1]§a■"
+    let title ="§9ULTIMA CONEXION"
     let message = ""
     LastDB.forEach((key, value) => {
       let plr = key
       const DateNow = Date.now()
       const SecondPlayed =Math.ceil((DateNow - value) / 1000);
-      message += `\n§1 | §3${plr} §1| §bno entra desde hace§1 -`
+      message += `\n§9 | §3${plr} §1| §bno entra desde hace§1 -`
       if (SecondPlayed >= 86400) {
         let day = Math.floor(SecondPlayed / 86400)
         message += ` §f${day} §bdias,`
@@ -34,9 +34,9 @@ Untravel.cmd.add({
         message += ` §f${minute % 60} §bminutos,`
       }
       let second = SecondPlayed
-      message += ` §f${second % 60} §bsegundos`
+      message += ` §f${second % 60} §bsegundos\n§9--------------------`
     })
-    const form = new ActionFormData().title(`${title}`).body(`${message}`).button("§9Ok")
+    const form = new ActionFormData().title(`${title}`).body(`${message}`).button("§l§b<<*>>")
     player.sendMessage(Config.FormMessage)
     let res = await ForceOpen(player, form)
     if (!res.canceled){
